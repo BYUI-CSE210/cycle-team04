9a6a1358d9a7b76cc8b3dc6dc2ce17cad0f80c56
Nelson Georges <georson00@gmail.com>
2022-06-29 23:38:34 -0400

    updates

--- cycle/game/scripting/handle_collisions_action.py
+++ cycle/game/scripting/handle_collisions_action.py
@@ -1,15 +1,16 @@
 import constants
-from game.casting.actor import Actor
+from game.casting.actor import Actor                                       
 from game.scripting.action import Action
 from game.shared.point import Point
+from game.casting.score1 import Score1
+from game.casting.score2 import Score2
 
 
 class HandleCollisionsAction(Action):
     """
     An update action that handles interactions between the actors.
 
-    The responsibility of HandleCollisionsAction is to handle the situation when the snake collides
-    with the food, or the snake collides with its segments, or the game is over.
+    The responsibility of HandleCollisionsAction is to handle the situation when a snake collides with its segments and the segments of the other snake, or the game is over.
 
     Attributes:
         _is_game_over (boolean): Whether or not the game is over.
@@ -18,6 +19,7 @@ class HandleCollisionsAction(Action):
     def __init__(self):
         """Constructs a new HandleCollisionsAction."""
         self._is_game_over = False
+
 
     def execute(self, cast, script):
         """Executes the handle collisions action.
@@ -31,25 +33,43 @@ class HandleCollisionsAction(Action):
             self._handle_game_over(cast)
     
     def _handle_segment_collision(self, cast):
-        """Sets the game over flag if the snake collides with one of its segments.
+        """Sets the game over flag if a snake collides with its segments or the segments of the other snake.
         
         Args:
             cast (Cast): The cast of Actors in the game.
         """
+        score1 = cast.get_first_actor("score1")
+        score2 = cast.get_first_actor("score2")
         all_cycles = cast.get_actors("cycles")
-        segments = []
+        cycle1 = cast.get_actors("cycles")[0]
+        cycle2 = cast.get_actors("cycles")[1]
+        segments_1 = []
+        segments_2 = []
         heads = []
         for cycle in all_cycles:
             heads.append(cycle.get_segments()[0])
-            segments += cycle.get_segments()[1:]
-        
-        for segment in segments:
+            segments_1 += cycle1.get_segments()[1:]
+            segments_2 += cycle2.get_segments()[1:]
+       
+        for segment1 in segments_1:
             for head in heads:
-                if head.get_position().equals(segment.get_position()):
+                if head.get_position().equals(segment1.get_position()):
                     self._is_game_over = True
+                    score2.add_points(1)
+               
+                    
+
+        for segment2 in segments_2:
+            for head in heads:
+                if head.get_position().equals(segment2.get_position()):
+                    self._is_game_over = True
+                    score1.add_points(1)
+               
+                    
+                    
                     
     def _handle_game_over(self, cast):
-        """Shows the 'game over' message and turns the snake and food white if the game is over.
+        """Shows the 'game over' message and turns the snakes white if the game is over.
         
         Args:
             cast (Cast): The cast of Actors in the game.
@@ -66,3 +86,5 @@ class HandleCollisionsAction(Action):
             message.set_text("Game Over!")
             message.set_position(position)
             cast.add_actor("messages", message)
+
+      
